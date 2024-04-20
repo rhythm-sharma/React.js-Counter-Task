9c9173d482a43b4cbd22d502574d35ae296e62fb
rhythm-sharma <rrhythmsharma@gmail.com>
2024-04-20 23:23:30 +0530

    added: reducers for increment, decreament and incrementByAmount

--- lib/redux/slices/counterSlice/counterSlice.ts
+++ lib/redux/slices/counterSlice/counterSlice.ts
@@ -1,5 +1,5 @@
 /* Core */
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const initialState: CounterSliceState = {
   value: 0,
@@ -10,9 +10,21 @@ export const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
-    // increment, decreament, incrementByAmount loginc here
+    // increment, decreament, incrementByAmount login here
+    incrementCounter: (state) => {
+      state.value += 1;
+    },
+    decreamentCounter: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmountCounter: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    },
   },
 });
+
+export const { incrementCounter, decreamentCounter, incrementByAmountCounter } =
+  counterSlice.actions;
 
 /* Types */
 export interface CounterSliceState {
