b94330408b79ba4056d29c6dbe7330676877bd58
rhythm-sharma <rrhythmsharma@gmail.com>
2024-04-20 23:27:39 +0530

    updated: dispatch actions on UI layer

--- app/components/Counter/Counter.tsx
+++ app/components/Counter/Counter.tsx
@@ -1,26 +1,68 @@
 "use client";
 
-/* Core */
-import { useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 
 /* Instruments */
-import { useSelector, selectCount } from "@/lib/redux";
+import {
+  useSelector,
+  useDispatch,
+  selectCount,
+  incrementCounter,
+  decreamentCounter,
+  incrementByAmountCounter,
+  incrementIfOddAsync,
+} from "@/lib/redux";
 import styles from "./counter.module.css";
 
 export const Counter = () => {
+  const dispatch = useDispatch();
+
   const count = useSelector(selectCount);
 
   // Create a state named incrementAmount
+  const [incrementAmount, setIncrementAmount] = useState<number>(0);
+
+  const disabledDecrementButton = useMemo(() => count <= 0, [count]);
+
+  const handleSetIncrementAmount = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const newIncrementAmount = Number(event.target.value);
+      if (newIncrementAmount < 0) {
+        return;
+      }
+      setIncrementAmount(newIncrementAmount);
+    },
+    []
+  );
+
+  const handleIncrement = useCallback(() => {
+    // dispatch event to increment count by 1
+    dispatch(incrementCounter());
+  }, []);
+
+  const handleDecreament = useCallback(() => {
+    // dispatch event to decrease count by 1
+    dispatch(decreamentCounter());
+  }, []);
+
+  const handleIncrementByAmount = useCallback(() => {
+    // dispatch event to add incrementAmount to count
+    dispatch(incrementByAmountCounter(incrementAmount));
+  }, [incrementAmount]);
+
+  const handleIncrementIfOddAsync = useCallback(() => {
+    // dispatch event to add incrementAmount only if count is odd
+    dispatch(incrementIfOddAsync(incrementAmount));
+  }, [incrementAmount]);
 
   return (
     <div>
       <div className={styles.row}>
         <button
+          disabled={disabledDecrementButton}
           className={styles.button}
           aria-label="Decrement value"
-          onClick={() => {
-            // dispatch event to decrease count by 1
-          }}
+          onClick={handleDecreament}
         >
           -
         </button>
@@ -28,29 +70,23 @@ export const Counter = () => {
         <button
           className={styles.button}
           aria-label="Increment value"
-          onClick={() => {
-            // dispatch event to increment count by 1
-          }}
+          onClick={handleIncrement}
         >
           +
         </button>
       </div>
       <div className={styles.row}>
-        <input className={styles.textbox} aria-label="Set increment amount" />
-        <button
-          className={styles.button}
-          onClick={() => {
-            // dispatch event to add incrementAmount to count
-          }}
-        >
+        <input
+          type="number"
+          value={incrementAmount}
+          className={styles.textbox}
+          aria-label="Set increment amount"
+          onChange={handleSetIncrementAmount}
+        />
+        <button className={styles.button} onClick={handleIncrementByAmount}>
           Add Amount
         </button>
-        <button
-          className={styles.button}
-          onClick={() => {
-            // dispatch event to add incrementAmount only if count is odd
-          }}
-        >
+        <button className={styles.button} onClick={handleIncrementIfOddAsync}>
           Add If Odd
         </button>
       </div>
