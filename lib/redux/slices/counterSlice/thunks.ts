b7ee7c91a103eb4a767645626ef5f282e8a4b19d
rhythm-sharma <rrhythmsharma@gmail.com>
2024-04-20 23:25:12 +0530

    added: logic for click on odd button increment

--- lib/redux/slices/counterSlice/thunks.ts
+++ lib/redux/slices/counterSlice/thunks.ts
@@ -1,8 +1,11 @@
 /* Instruments */
-import type { ReduxThunkAction } from "@/lib/redux";
+import { incrementByAmountCounter, type ReduxThunkAction } from "@/lib/redux";
 
-export const incrementIfOddAsync =
-  (amount: number): ReduxThunkAction =>
-  () => {
+export const incrementIfOddAsync = (amount: number): ReduxThunkAction => {
+  return (dispatch) => {
     // update only if currentValue is odd
+    if (amount % 2 !== 0) {
+      dispatch(incrementByAmountCounter(amount));
+    }
   };
+};
